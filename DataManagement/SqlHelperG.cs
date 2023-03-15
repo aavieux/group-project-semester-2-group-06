d84c8a036784d965a6c0f803c529a41ca198a3b1
Grigoriy Nikolov <g.nikolov@student.fontys.nl>
2023-03-15 17:12:07 +0100

    added form for managers to maange stock and adding products

---
+++ DataManagement/SqlHelperG.cs
@@ -0,0 +1,81 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace DataManagement;
+
+public class SqlHelperG
+{
+    SqlCommand cmd;
+    SqlDataAdapter reader;
+    DataTable datatable1;
+    DataTable datatable2;
+    string connection = "Data Source=mssqlstud.fhict.local;Initial Catalog=dbi501511_bazaar;User ID=dbi501511_Bazaar;Password=Samocska";
+    string query;
+
+    public void AddProduct(string name, string category, string description)
+    {
+        query = "INSERT INTO dbo.Product (name, amount, category, description) VALUES (@name, @amount, @category, @description)";
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@amount", 0);
+                cmd.Parameters.AddWithValue("@category", category);
+                cmd.Parameters.AddWithValue("@description", description);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+    public void RemoveProduct(int id)
+    {
+        query = "Delete from dbo.Product where id=@id";
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+    public void IncreaseStock(int id, int amount)
+    {
+        query = "UPDATE dbo.Product SET amount = @amount WHERE id=@id";
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@amount", amount);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+}
---
+++ Project_Media_Bazaar/AddProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Domain;
+namespace Project_Media_Bazaar
+{
+    public partial class AddProduct : Form
+    {
+        Management manager;
+        public AddProduct(Management manager)
+        {
+            InitializeComponent();
+            this.manager = manager;
+        }
+
+        private void bAddProduct_Click(object sender, EventArgs e)
+        {
+            manager.AddProduct(tbName.Text, (Category)cbProductCategory.SelectedIndex, tbDescription.Text);
+            this.Close();
+        }
+    }
+}
