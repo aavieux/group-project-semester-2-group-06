86c26dc11da081bc110f5c644642df034cea0781
Grigoriy Nikolov <g.nikolov@student.fontys.nl>
2023-03-16 12:44:00 +0100

    Now everything should be fine !

--- Project_Media_Bazaar/ManagementForm.Designer.cs
+++ Project_Media_Bazaar/ManagementForm.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(1153, 854);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1171, 241);
+            button1.Name = "button1";
+            button1.Size = new Size(298, 155);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1171, 402);
+            button2.Name = "button2";
+            button2.Size = new Size(298, 149);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1171, 557);
+            button3.Name = "button3";
+            button3.Size = new Size(298, 151);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1171, 714);
+            button4.Name = "button4";
+            button4.Size = new Size(298, 152);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // ManagementForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1481, 885);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "ManagementForm";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
