0965ba440b72210f71c2238e35504c48c92a6c93
Mario Petkov <i502277@fhict.nl>
2023-03-17 13:30:00 +0100

    ProductInformation form.

---
+++ Domain/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Company
+    {
+        public List<Product> products { get; private set; }
+        public Company()
+        {
+            products = new List<Product>();
+        }
+        public Product? findProductById(int id)
+        {
+            foreach(Product product in products)
+            {
+                if(product.Id == id)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Amount { get; private set; }
+        public Category Category { get; private set; }
+        public string Description { get; private set; }
+        public Product(int id, string name, int amount, string category, string description) {
+            Id = id;
+            Name = name;
+            Amount = amount;
+            Category = Enum.Parse<Category>(category);
+            Description = description;
+        }
+    }
+}
