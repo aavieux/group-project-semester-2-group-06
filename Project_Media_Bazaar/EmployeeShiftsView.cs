3109c9e7d12ca494fdaed6d18768a10a361ab04d
Denitsa Draganova <502422@student.fontys.nl>
2023-05-16 13:57:21 +0200

    gh

---
+++ Project_Media_Bazaar/EmployeeShiftsView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Media_Bazaar
+{
+    public partial class EmployeeShiftsView : Form
+    {
+        public EmployeeShiftsView()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            ShiftForm shiftForm = new ShiftForm(selectedDate);
+            shiftForm.Show();
+        }
+    }
+}
---
+++ Project_Media_Bazaar/ShiftForm.cs
@@ -0,0 +1,53 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Project_Media_Bazaar
+{
+    public partial class ShiftForm : Form
+    {
+        DataAccessEmployeeDashboard dataacces = new DataAccessEmployeeDashboard("Server=mssqlstud.fhict.local;Database=dbi501511_bazaar;User Id=dbi501511_Bazaar;Password=Samocska;");
+        private DateTime selectedDate;
+        string formattedDate;
+        List<Shift> shifts;
+        public ShiftForm(DateTime date)
+        {
+
+            InitializeComponent();
+            lbEmployeeShifts.Items.Clear();
+           formattedDate  = date.ToString("dd.M.yyyy");
+            shifts = dataacces.GetAllShiftsFromDB();
+          
+               
+            foreach (var shift in shifts)
+            {
+                if(shift.GetInfo().Contains(formattedDate)) {
+                    lbEmployeeShifts.Items.Add(shift.GetInfo());
+                }
+            
+                  
+
+                
+            }
+
+        }
+
+        private void ShiftForm_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
