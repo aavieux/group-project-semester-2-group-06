b1854d849b2bacadfde05c9c71b69e4a680c4ae3
Aleksandar <a.garkov@student.fontys.nl>
2023-06-23 09:25:26 +0200

    alex

--- Project_Media_Bazaar/EmployeeShiftsView.Designer.cs
+++ Project_Media_Bazaar/EmployeeShiftsView.Designer.cs
@@ -29,32 +29,62 @@
         private void InitializeComponent()
         {
             monthCalendar1 = new MonthCalendar();
+            search_lbl = new Label();
+            comboBox1 = new ComboBox();
             SuspendLayout();
             // 
             // monthCalendar1
             // 
-            monthCalendar1.CalendarDimensions = new Size(3, 2);
+            monthCalendar1.CalendarDimensions = new Size(5, 2);
             monthCalendar1.Dock = DockStyle.Fill;
             monthCalendar1.Location = new Point(0, 0);
             monthCalendar1.Margin = new Padding(8, 7, 8, 7);
+            monthCalendar1.MaximumSize = new Size(689, 309);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 0;
             monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+            // 
+            // search_lbl
+            // 
+            search_lbl.AutoSize = true;
+            search_lbl.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            search_lbl.Location = new Point(700, 9);
+            search_lbl.Name = "search_lbl";
+            search_lbl.Size = new Size(150, 28);
+            search_lbl.TabIndex = 11;
+            search_lbl.Text = "Search by name";
+            // 
+            // comboBox1
+            // 
+            comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(700, 49);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(435, 36);
+            comboBox1.TabIndex = 12;
+            comboBox1.TextChanged += comboBox1_TextChanged;
             // 
             // EmployeeShiftsView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 321);
+            ClientSize = new Size(1147, 318);
+            Controls.Add(comboBox1);
+            Controls.Add(search_lbl);
             Controls.Add(monthCalendar1);
             Margin = new Padding(3, 2, 3, 2);
             Name = "EmployeeShiftsView";
             Text = "EmployeeShiftsView";
+            Load += EmployeeShiftsView_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private MonthCalendar monthCalendar1;
+        private Label search_lbl;
+        private ComboBox comboBox1;
     }
 }
--- Project_Media_Bazaar/EmployeeShiftsView.cs
+++ Project_Media_Bazaar/EmployeeShiftsView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataManagement;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Project_Media_Bazaar
 {
     public partial class EmployeeShiftsView : Form
     {
+        DataAccessEmployeeDashboard dataAccessEmployeeDashboard;
         public EmployeeShiftsView()
         {
             InitializeComponent();
+
+            dataAccessEmployeeDashboard = new DataAccessEmployeeDashboard(
+                "Server=mssqlstud.fhict.local;Database=dbi501511_bazaar;User Id=dbi501511_Bazaar;Password=Samocska;");
+
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
@@ -28,6 +36,26 @@ namespace Project_Media_Bazaar
         {
             EmployeeShiftsView employeeShiftsView = new EmployeeShiftsView();
             employeeShiftsView.ShowDialog();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            //string searchInput = comboBox1.Text;
+            //comboBox1.DataSource = dataAccessEmployeeDashboard.GetEmployeesFromDB();
+            //// Filter the data source based on the search input
+            //List<string> filteredValues = comboBox1.DataSource
+            //    .Where(value => value.StartsWith(searchInput, StringComparison.OrdinalIgnoreCase))
+            //    .ToList();
+
+            //comboBox1.DataSource = filteredValues;
+
+            //// Show the drop-down list
+            //comboBox1.DroppedDown = true;
+        }
+
+        private void EmployeeShiftsView_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
